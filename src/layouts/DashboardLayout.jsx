d71163b734a40af9c19c70347ed7adbe1606a869
mh-shihan <mahmodul.no.1@gmail.com>
2025-03-30 23:05:42 +0600

    Created dashboard layout

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,5 +1,21 @@
+import { Outlet } from "react-router-dom";
+import Sidebar from "../components/dashboard/sidebar/Sidebar";
+
 const DashboardLayout = () => {
-  return <div>I am From Dashboard Layout</div>;
+  return (
+    <div className="relative min-h-screen md:flex bg-white">
+      <p>Hello Dashboard</p>
+      {/* Left Side: Sidebar Component */}
+      <Sidebar />
+      {/* Right Side: Dashboard Dynamic Content */}
+      <div className="flex-1  md:ml-64">
+        <div className="p-5">
+          {/* Outlet for dynamic contents */}
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
