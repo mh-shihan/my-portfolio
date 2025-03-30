b72ce80de501372e92338c004719b22d7b1635a1
mh-shihan <mahmodul.no.1@gmail.com>
2025-03-30 22:51:10 +0600

    Make menu item component

---
+++ src/components/dashboard/menu/MenuItem.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router-dom";
+
+const MenuItem = ({ label, address, icon: Icon }) => {
+  return (
+    <NavLink
+      to={address}
+      end
+      className={({ isActive }) =>
+        `flex items-center px-4 py-2 my-5  transition-colors duration-300 transform  text-zinc-300 hover:bg-[#B9FF00]  hover:text-gray-700 ${
+          isActive ? "bg-[#B9FF00]  text-zinc-700" : "text-zinc-300"
+        }`
+      }
+    >
+      <Icon className="w-5 h-5" />
+
+      <span className="mx-4 font-medium">{label}</span>
+    </NavLink>
+  );
+};
+
+export default MenuItem;
