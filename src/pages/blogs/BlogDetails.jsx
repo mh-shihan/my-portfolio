c41dd7404966f9ceeb9030451379d72d0a98a553
mh-shihan <mahmodul.no.1@gmail.com>
2025-04-15 00:49:22 +0600

    Make scroll page to top component

--- src/pages/blogs/BlogDetails.jsx
+++ src/pages/blogs/BlogDetails.jsx
@@ -1,12 +1,18 @@
 import { useLoaderData } from "react-router-dom";
 import BlogHeader from "./BlogHeader";
+import BlogContainer from "../../shared/BlogContainer";
 
 const BlogDetails = () => {
   const blog = useLoaderData();
   return (
     <div>
       <BlogHeader title={blog.title} description={blog.short_description} />
-      {blog.title}
+      <BlogContainer>
+        <div className=" flex flex-col items-center xl:grid gird-cols-12 gap-6 xl:gap-12">
+          <div className="col-span-6"></div>
+          <div className="col-span3"></div>
+        </div>
+      </BlogContainer>
     </div>
   );
 };
