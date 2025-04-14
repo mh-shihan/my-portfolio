6d0c0c9baaa464c341a4a6572c19a332b7f71869
mh-shihan <mahmodul.no.1@gmail.com>
2025-04-15 00:58:53 +0600

    Added ScrollPageToTop in MainLaout and BlogLayout

--- src/layouts/BlogLayout.jsx
+++ src/layouts/BlogLayout.jsx
@@ -1,15 +1,21 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../common/Footer";
 import Navbar from "../common/Navbar";
+import ScrollPageToTop from "../components/ScrollPageToTop";
+import ScrollToTop from "../components/ScrollToTop";
 
 const BlogLayout = () => {
   return (
     <div>
+      <ScrollPageToTop />
+
       <Navbar />
 
       <Outlet />
 
       <Footer />
+
+      <ScrollToTop />
     </div>
   );
 };
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,16 +2,18 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../common/Navbar";
 import Footer from "../common/Footer";
 import ScrollToTop from "../components/ScrollToTop";
+import ScrollPageToTop from "../components/ScrollPageToTop";
 
 const MainLayout = () => {
   return (
     <div className="interFont">
-      <Navbar></Navbar>
+      <ScrollPageToTop />
+      <Navbar />
       <div className="pt-16 max-w-[2520px] mx-auto px-0 md:px-6 xl:px-20">
         <Outlet></Outlet>
       </div>
-      <Footer></Footer>
-      <ScrollToTop>ss</ScrollToTop>
+      <Footer />
+      <ScrollToTop />
     </div>
   );
 };
