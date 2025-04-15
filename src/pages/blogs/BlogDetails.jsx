641e11695c987bcd5fd45b160d2fe6ce8c0bd4e3
mh-shihan <mahmodul.no.1@gmail.com>
2025-04-15 22:16:46 +0600

    Complted blog details header section

--- src/pages/blogs/BlogDetails.jsx
+++ src/pages/blogs/BlogDetails.jsx
@@ -1,6 +1,8 @@
 import { useLoaderData } from "react-router-dom";
 import BlogHeader from "./BlogHeader";
 import BlogContainer from "../../shared/BlogContainer";
+import { BsCalendarDateFill } from "react-icons/bs";
+import { TbCategoryFilled } from "react-icons/tb";
 
 const BlogDetails = () => {
   const blog = useLoaderData();
@@ -8,9 +10,31 @@ const BlogDetails = () => {
     <div>
       <BlogHeader title={blog.title} description={blog.short_description} />
       <BlogContainer>
-        <div className=" flex flex-col items-center xl:grid gird-cols-12 gap-6 xl:gap-12">
-          <div className="col-span-6"></div>
-          <div className="col-span3"></div>
+        <div className=" flex xl:grid xl:grid-cols-12 gap-6 xl:gap-10 ">
+          <div className="xl:col-span-9 mr-10  ">
+            <div className="border-b border-[#C4F000] pb-8 space-y-6">
+              <img
+                className="rounded-sm w-full"
+                src={blog.img}
+                alt={blog.title}
+              />
+              <h2 className="text-white text-3xl md:text-4xl font-medium">
+                {blog.title}
+              </h2>
+              <div className="flex gap-10">
+                <p className="flex  items-center justify-center gap-2 text-[#1d1d26]  uppercase bg-[#C4F000] px-6 py-2 rounded-xl">
+                  <BsCalendarDateFill /> {blog.posted_date}
+                </p>
+                <p className="flex  items-center justify-center gap-2 text-[#1d1d26]  uppercase bg-[#C4F000] px-6 py-2 rounded-xl">
+                  <TbCategoryFilled />
+                  {blog.category}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="xl:col-span-3 border">
+            <img className="rounded-sm" src={blog.img} alt={blog.title} />
+          </div>
         </div>
       </BlogContainer>
     </div>
