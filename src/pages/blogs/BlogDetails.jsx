37e5b29389300557eaeaa392e8ce83465e716647
mh-shihan <mahmodul.no.1@gmail.com>
2025-04-18 20:00:01 +0600

    Resposive the blog details page

--- src/pages/blogs/BlogDetails.jsx
+++ src/pages/blogs/BlogDetails.jsx
@@ -6,6 +6,7 @@ import { TbCategoryFilled } from "react-icons/tb";
 import { useEffect, useState } from "react";
 import Prism from "prismjs";
 import MarkdownPreview from "@uiw/react-markdown-preview"; //Very Important
+import { Fullscreen } from "lucide-react";
 
 const BlogDetails = () => {
   const blog = useLoaderData();
@@ -21,22 +22,22 @@ const BlogDetails = () => {
     <div>
       <BlogHeader title={blog.title} description={blog.short_description} />
       <BlogContainer>
-        <div className=" flex xl:grid xl:grid-cols-12 gap-6 xl:gap-10 ">
-          <div className="xl:col-span-9 mr-10">
-            <div className="border-b border-[#C4F000] pb-8 space-y-6 ">
+        <div className=" flex flex-col xl:flex-row xl:grid xl:grid-cols-12 gap-6 xl:gap-12 ">
+          <div className="w-full xl:col-span-9 mr-10">
+            <div className="border-b border-[#C4F000] pb-8 space-y-6 xl:max-w-full  overflow-hidden">
               <img
-                className="rounded-sm w-full"
+                className="rounded-sm w-full h-auto max-w-full object-cover aspect-video"
                 src={blog.img}
                 alt={blog.title}
               />
               <h2 className="text-white text-3xl md:text-4xl font-medium">
                 {blog.title}
               </h2>
-              <div className="flex gap-10">
-                <p className="flex  items-center justify-center gap-2 text-[#1d1d26]  uppercase bg-[#C4F000] px-6 py-2 rounded-xl">
+              <div className="flex flex-col md:flex-row gap-4 md:gap-10">
+                <p className="flex text-sm md:text-base items-center justify-center gap-2 text-[#1d1d26]  uppercase bg-[#C4F000] px-6 py-2 rounded-xl">
                   <BsCalendarDateFill /> {blog.posted_date}
                 </p>
-                <p className="flex  items-center justify-center gap-2 text-[#1d1d26]  uppercase bg-[#C4F000] px-6 py-2 rounded-xl">
+                <p className="flex text-sm md:text-base items-center justify-center gap-2 text-[#1d1d26]  uppercase bg-[#C4F000] px-6 py-2 rounded-xl">
                   <TbCategoryFilled />
                   {blog.category}
                 </p>
@@ -44,10 +45,14 @@ const BlogDetails = () => {
             </div>
 
             {/* Markdown Content */}
-            <div className="border">
+            <div className=" border max-w-full px-4 xl:px-0 overflow-x-hidden ">
               <MarkdownPreview
                 source={blogContent}
-                style={{ padding: 16 }}
+                style={{
+                  padding: 16,
+                  overflow: "scroll",
+                  maxWidth: Fullscreen,
+                }}
                 rehypeRewrite={(node, index, parent) => {
                   if (
                     node.tagName === "a" &&
@@ -62,7 +67,11 @@ const BlogDetails = () => {
           </div>
 
           <div className="xl:col-span-3 border">
-            <img className="rounded-sm" src={blog.img} alt={blog.title} />
+            <img
+              className="rounded-sm w-full h-auto object-cover"
+              src={blog.img}
+              alt={blog.title}
+            />
           </div>
         </div>
       </BlogContainer>
