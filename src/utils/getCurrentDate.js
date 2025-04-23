6e5cf953dd61049e92c3f08b95d06f6d7ebd1876
mh-shihan <mahmodul.no.1@gmail.com>
2025-04-23 08:37:24 +0600

    Update get current details

--- src/utils/getCurrentDate.js
+++ src/utils/getCurrentDate.js
@@ -3,7 +3,7 @@ const getCurrentDate = () => {
   const year = date.getFullYear();
   const month = date.toLocaleDateString("en-US", { month: "short" }); // Months are zero-based
   const day = String(date.getDate()).padStart(2, "0"); // Days are one-based
-  const formattedDate = `${day}-${month}-${year}`;
+  const formattedDate = `${day} ${month} ${year}`;
   //   console.log(formattedDate);
 
   return formattedDate;
